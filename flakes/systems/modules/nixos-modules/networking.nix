86895d3c3b57cecf68df1b74f8bb03a26689a3c1
Laurens <laurens@kruis.name>
2026-05-20 16:16:24 +0200

    Switch from networkmanager to networkd+iwd+impala

--- flakes/systems/modules/nixos-modules/networking.nix
+++ flakes/systems/modules/nixos-modules/networking.nix
@@ -1,5 +1,28 @@
 { ... }: {
 	flake.nixosModules.networking = { config, pkgs, lib, ... }: {	
-		networking.networkmanager.enable = true;
+		# Enable networkd
+		systemd.network.enable = true;
+		networking.useNetworkd = true;
+		networking.useDHCP = true;
+	
+		# disable wpa_supplicant, enable iwd
+		networking.wireless.enable = false;
+		networking.wireless.iwd = {
+			enable = true;
+			settings = {
+				Network = {
+					EnableIPv6 = true;
+				};
+
+				Settings = {
+					AutoConnect = true;
+				};
+			};
+		};
+
+		# Nice tui for wifi
+		environment.systemPackages = [
+			pkgs.impala
+		];
 	};
 }
