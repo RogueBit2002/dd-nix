293b84a70e807c74909c3ba17a2112811f88b381
RogueBit2002 <laurens@kruis.name>
2026-09-10 13:36:39 +0200

    .

---
+++ modules/hosts/common.nix
@@ -0,0 +1,61 @@
+{ ... }: {
+	flake.nixosModules.common = { pkgs, lib, ... }: {
+		
+		services.dbus.implementation = "broker";
+		programs.ssh.startAgent = true;
+
+		boot.kernelPackages = lib.mkForce pkgs.linuxPackages_7_1;
+		boot.loader.systemd-boot.enable = true;
+ 		boot.loader.efi.canTouchEfiVariables = true;
+
+
+		environment.systemPackages = with pkgs; [
+			wget
+			dig
+			git
+			git-credential-manager
+			age
+			sops
+			unzip
+			lm_sensors
+		];
+
+		services.greetd = {
+			enable = true;
+			useTextGreeter = true;
+			settings = {
+				default_session = {
+					command = "${lib.getExe pkgs.tuigreet} --remember --remember-user-session --time";
+        			user = "greeter";
+				};
+			};
+		};
+
+		security.rtkit.enable = true;
+		services.pipewire = {
+			enable = true;
+			alsa.enable = true;
+			alsa.support32Bit = true;
+			pulse.enable = true;
+		};
+
+		time.timeZone = "Europe/Amsterdam";
+
+		i18n.defaultLocale = "en_US.UTF-8";
+		i18n.extraLocaleSettings = {
+			LC_ADDRESS = "nl_NL.UTF-8";
+			LC_IDENTIFICATION = "nl_NL.UTF-8";
+			LC_MEASUREMENT = "nl_NL.UTF-8";
+			LC_MONETARY = "nl_NL.UTF-8";
+			LC_NAME = "nl_NL.UTF-8";
+			LC_NUMERIC = "nl_NL.UTF-8";
+			LC_PAPER = "nl_NL.UTF-8";
+			LC_TELEPHONE = "nl_NL.UTF-8";
+			LC_TIME = "nl_NL.UTF-8";
+		};
+
+		security.sudo.extraConfig = ''
+			Defaults lecture = never
+		'';
+	};
+}
--- modules/hosts/ganymede/configuration.nix
+++ modules/hosts/ganymede/configuration.nix
@@ -2,9 +2,10 @@
 	flake.nixosConfigurations.ganymede = inputs.nixpkgs.lib.nixosSystem {
 		modules = [
 			self.nixosModules.ganymede-hardware	
+			self.nixosModules.common
 
 			inputs.impermanence.nixosModules.impermanence
-			inputs.home-manager.nixosModules.default
+			self.nixosModules.home-manager
 
 			self.nixosModules.sops
 			self.nixosModules.nix
@@ -18,16 +19,9 @@
 	
 				networking.hostName = "ganymede";
 				
-				services.dbus.implementation = "broker";
 				
 				services.fwupd.enable = true;
 
-				programs.ssh.startAgent = true;
-
-				environment.pathsToLink = [
-					"/share/applications" # home-manager
-					"/share/xdg-desktop-portal" # home-manager
-				];
 
 				environment.persistence."/persist" = {
 					enable = true;
@@ -43,65 +37,12 @@
 					];
 				};
 				
-				security.sudo.extraConfig = ''
-					Defaults lecture = never
-				'';
-
 
 
 				systemd.network.enable = true;
 				networking.useNetworkd = true;
 				networking.useDHCP = true;
-
-	environment.systemPackages = with pkgs; [
-			wget
-			dig
-			git
-			git-credential-manager
-			age
-			sops
-			unzip
-			lm_sensors
-		];
-		
-		boot.kernelPackages = lib.mkForce pkgs.linuxPackages_7_1;
-		boot.loader.systemd-boot.enable = true;
- 		boot.loader.efi.canTouchEfiVariables = true;
-
-		services.greetd = {
-			enable = true;
-			useTextGreeter = true;
-			settings = {
-				default_session = {
-					command = "${lib.getExe pkgs.tuigreet} --remember --remember-user-session --time";
-        			user = "greeter";
-				};
-			};
-		};
-
-		security.rtkit.enable = true;
-		services.pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-		};
-
-		time.timeZone = "Europe/Amsterdam";
-
-		i18n.defaultLocale = "en_US.UTF-8";
-		i18n.extraLocaleSettings = {
-			LC_ADDRESS = "nl_NL.UTF-8";
-			LC_IDENTIFICATION = "nl_NL.UTF-8";
-			LC_MEASUREMENT = "nl_NL.UTF-8";
-			LC_MONETARY = "nl_NL.UTF-8";
-			LC_NAME = "nl_NL.UTF-8";
-			LC_NUMERIC = "nl_NL.UTF-8";
-			LC_PAPER = "nl_NL.UTF-8";
-			LC_TELEPHONE = "nl_NL.UTF-8";
-			LC_TIME = "nl_NL.UTF-8";
-		};
-
+	
 		programs.steam.enable = true;
 
 		hardware.bluetooth.enable = true;
@@ -122,9 +63,6 @@
 		};
 	
 		home-manager = {
-			useGlobalPkgs = true;
-			useUserPackages = true;
-
 			users.roguebit = { ... }: {
 				imports = [
 					self.homeModules.window-manager
@@ -134,6 +72,7 @@
 				home.packages = with pkgs; [
 					impala
 				];
+
 				home.stateVersion = "26.05";
 			};
 		};
